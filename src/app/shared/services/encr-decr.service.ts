5631313b6eee29090e4ea93e64c86844ecfda68e
Sreejith <ssreedharan@acr.org>
2019-02-10 12:48:27 +0530

    Code changes

--- src/app/shared/services/encr-decr.service.ts
+++ src/app/shared/services/encr-decr.service.ts
@@ -10,18 +10,18 @@ export class EncrDecrService {
   constructor() { }
 
   encryptPassword(value) {
-    return CryptoJS.AES.encrypt(this.passPhrase, value).toString();
+    return CryptoJS.AES.encrypt(value, this.passPhrase).toString();
   }
 
   decryptPassword(value) {
-    return CryptoJS.AES.decrypt(this.passPhrase, value).toString();
+    return CryptoJS.AES.decrypt(value, this.passPhrase).toString(CryptoJS.enc.Utf8);
   }
 
   encryptMailContent(value) {
-    return CryptoJS.AES.encrypt(this.emailContentPhrase, value).toString();
+    return CryptoJS.AES.encrypt(value, this.emailContentPhrase).toString();
   }
 
   decryptMailContent(value) {
-    return CryptoJS.AES.decrypt(this.emailContentPhrase, value).toString();
+    return CryptoJS.AES.decrypt(value, this.emailContentPhrase).toString(CryptoJS.enc.Utf8);
   }
 }
